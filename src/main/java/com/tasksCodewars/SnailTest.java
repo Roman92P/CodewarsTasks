066c4eacf7ed8647c19f84761b32c285f956ad20
Roman <roman.paszkow@gmail.com>
2021-05-03 18:52:13 +0200

    correct snail Class

--- src/main/java/com/tasksCodewars/SnailTest.java
+++ src/main/java/com/tasksCodewars/SnailTest.java
@@ -7,61 +7,77 @@ import org.apache.commons.lang3.ArrayUtils;
 public class SnailTest {
 
 	public static void main(String[] args) {
-
-		int one[] = {   1, 2,  3,  4 };
-		int two[] = {   5, 6,  7,  8 };
-		int three[] = { 9, 10, 11, 12};
-		int four[] = { 13, 14, 15, 16};
-		int five[] = { 17, 18, 19, 20};
-
-		int[][] array = { one, two, three, four, five };
 		
-//		array = deleteRightSide(array);
-//		
-//		System.out.println(array[0][2]);
-//		System.out.println(array[1][2]);
-//		System.out.println(array[2][2]);
-//		System.out.println(array[3][2]);
-//		System.out.println(array[4][2]);
-
+//		int one[] =  { 1, 2, 3, 9};
+//		int two[] =  { 4, 5, 6, 4};
+//		int three[] = {7, 8, 9, 1};
+//		int four[] = { 1, 2, 3, 4};
+		
+		int one[] =  { 1, 2, 3};
+		int two[] =  { 4, 5, 6};
+		int three[] = {7, 8, 9};
+		int four[] = { 1, 2, 3};
+	
+		
+		int[][] array = { one, two, three, four};
+		
 		int[] snail = snail(array);
 
 		for (int i : snail) {
 			System.out.print(i);
 		}
+		
 	}
-	
+
 	private static int[] snail(int[][] array) {
-		int[]result= {};
+		int[] result = {};
 		int[][] temp = Arrays.copyOf(array, array.length);
-		int laps = temp.length/2;
+		int laps = findNumberOfLaps(temp);
 		for (int i = 0; i < laps; i++) {
 			int[] firstLine = getFirstLine(temp);
 			temp = ArrayUtils.remove(temp, 0);
 			int[] lastReversedLine = getLastReversedLine(temp);
-			temp = ArrayUtils.remove(temp, temp.length-1);
+			temp = removeLastLine(temp);
 			int[] rightSide = getRightSide(temp);
 			temp = deleteRightSide(temp);
 			int[] leftReverseSide = getLeftReverseSide(temp);
 			temp = deleteLeftSide(temp);
-			
-			
+
 			int[] stepOne = ArrayUtils.addAll(firstLine, rightSide);
 			int[] stepTwo = ArrayUtils.addAll(stepOne, lastReversedLine);
 			int[] addAll = ArrayUtils.addAll(stepTwo, leftReverseSide);
-			
-			result = ArrayUtils.addAll(result, addAll);
 
+			result = ArrayUtils.addAll(result, addAll);
 		}
-		
+
+		return result;
+	}
+
+	private static int[][] removeLastLine(int[][] temp) {
+		if(temp.length>1) {
+			temp = ArrayUtils.remove(temp, temp.length - 1);
+		}
+		return temp;
+	}
+
+	private static int findNumberOfLaps(int[][] array) {
+		int result = 0;
+		int length = array.length;
+		if (length % 2 == 0) {
+			result = length / 2;
+		}
+		if (length % 2 != 0) {
+			result = length / 2 + 1;
+		}
 		return result;
 	}
 
 	private static int[][] deleteRightSide(int[][] array) {
 		for (int i = 0; i < array.length; i++) {
+			int arrILenght = array[i].length;
 			for (int j = 0; j < array[i].length; j++) {
-				if (ArrayUtils.indexOf(array[i], array[i][j]) == array[i].length-1) {
-					array[i] = ArrayUtils.removeElement(array[i], array[i][j]);
+				if (j == arrILenght-1) {
+					array[i] = ArrayUtils.remove(array[i], j);
 				}
 			}
 		}
@@ -83,7 +99,7 @@ public class SnailTest {
 		int[] result = {};
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array[i].length; j++) {
-				if (ArrayUtils.indexOf(array[i], array[i][j]) == 0) {
+				if (j== 0) {
 					result = addElToArray(result, array[i][j]);
 				}
 			}
@@ -95,18 +111,23 @@ public class SnailTest {
 	private static int[] getRightSide(int[][] array) {
 		int[] result = {};
 		for (int i = 0; i < array.length; i++) {
+			int arrILength = array[i].length;
 			for (int j = 0; j < array[i].length; j++) {
-				if (ArrayUtils.indexOf(array[i], array[i][j]) == array[i].length - 1) {
-					result = addElToArray(result, array[i][j]);
+				if (j==arrILength-1) {
+					result = ArrayUtils.add(result, array[i][j]);
 				}
+				
 			}
 		}
 		return result;
 	}
 
 	private static int[] getLastReversedLine(int[][] array) {
-		int[] arr = array[array.length - 1];
-		ArrayUtils.reverse(arr);
+		int[] arr = {};
+		if (array.length > 1) {
+			arr = array[array.length - 1];
+			ArrayUtils.reverse(arr);
+		}
 		return arr;
 	}
 
